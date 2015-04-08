855c7b56ff92efce90dc4953ebabc4aca07f5eb8
Scott Livingston <slivingston@cds.caltech.edu>
2015-04-07 22:47:41 -0700

    Improve LQR example for integrator_chains domain

--- domains/integrator_chains/fmrb_sci_examples/scripts/lqr.py
+++ domains/integrator_chains/fmrb_sci_examples/scripts/lqr.py
@@ -5,17 +5,45 @@ import roslib; roslib.load_manifest('dynamaestro')
 import rospy
 from dynamaestro.msg import VectorStamped
 
+from control import lqr
+import numpy as np
 
-class LQRController(rospy.Subscriber):
-    def __init__(self, intopic, outtopic):
+
+class StateFeedback(rospy.Subscriber):
+    def __init__(self, intopic, outtopic, K=None):
         rospy.Subscriber.__init__(self, outtopic, VectorStamped, self.read_state)
         self.intopic = rospy.Publisher(intopic, VectorStamped, queue_size=1)
+        self.K = K
 
     def read_state(self, vs):
-        self.intopic.publish(VectorStamped(point=[-(vs.point[0] + 2.4142*vs.point[1] + 2.4142*vs.point[2])]))
+        self.intopic.publish(VectorStamped(point=[-np.dot(self.K, np.asarray(vs.point))]))
+
+class LQRController(StateFeedback):
+    def __init__(self, intopic, outtopic,
+                 A=None, B=None, Q=None, R=None):
+        if A is None and B is None:
+            A = np.array([[0., 1, 0],
+                          [0,  0, 1],
+                          [0,  0, 0]])
+            B = np.array([[0.], [0], [1]])
+        if Q is None and R is None:
+            Q = np.diag([1.,1,1])
+            R = np.diag([1.])
+        K, S, E = lqr(A,B,Q,R)
+        StateFeedback.__init__(self, intopic, outtopic, K)
 
 
 if __name__ == "__main__":
     rospy.init_node("lqr", anonymous=True)
-    lqrc = LQRController("input", "output")
+
+    n = 1  # Number of output dimensions
+    m = 3  # Number of derivatives
+
+    A = np.diag(np.ones(m-1), k=1)
+    B = np.zeros((m, 1))
+    B[-1,0] = 1.0
+    Q = np.diag(np.ones(m))
+    R = np.diag([1.])
+
+    lqrc = LQRController("input", "output", A, B, Q, R)
     rospy.spin()
