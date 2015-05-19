bcd68c9adcfddd702b8c01cda33bb81d709c7c6a
Scott Livingston <slivingston@cds.caltech.edu>
2015-05-18 17:20:57 -0700

    Naive example controller for first domain; new Python package
    The example controller for the first domain visits all goals in order
    by repeated construction and application of LQR solutions to stabilize
    about the mean point in each "goal" polytope. Currently it ignore
    obstacles, but some easy method to go around them will be added.
    
    This changeset also introduces the fmrb Python package, which will
    both provide common and domain-specific routines and representations
    for reviewing and working with the benchmarks, but which is not
    intended to be part of the benchmarks per se.

--- domains/integrator_chains/fmrb_sci_examples/scripts/lqr.py
+++ domains/integrator_chains/fmrb_sci_examples/scripts/lqr.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import time
+import numpy as np
+import numpy.linalg as la
+from control import lqr
 
 import roslib; roslib.load_manifest('fmrb_sci_examples')
 import rospy
 from dynamaestro.msg import VectorStamped, Vector
 
-from control import lqr
-import numpy as np
+from fmrb import integrator_chains
 
 
-class StateFeedback(rospy.Subscriber):
+class StateFeedback:
     def __init__(self, intopic, outtopic, K=None):
-        rospy.Subscriber.__init__(self, outtopic, VectorStamped, self.read_state)
+        self.error = None
         self.intopic = rospy.Publisher(intopic, VectorStamped, queue_size=1)
+        self.outtopic = rospy.Subscriber(outtopic, VectorStamped, self.read_state)
         self.K = K
 
     def read_state(self, vs):
-        self.intopic.publish(VectorStamped(v=Vector(-np.dot(self.K, np.asarray(vs.v.point)))))
+        self.error = np.asarray(vs.v.point) - self.target_state
+        self.intopic.publish(VectorStamped(v=Vector(-np.dot(self.K, self.error))))
+
+    def unregister(self):
+        self.outtopic.unregister()
+        self.intopic.unregister()
 
 class LQRController(StateFeedback):
     def __init__(self, intopic, outtopic,
-                 A, B, Q=None, R=None):
+                 A, B, target=None, Q=None, R=None):
         if Q is None:
             Q = np.eye(A.shape[0])
         if R is None:
             R = np.eye(B.shape[1])
+        self.target_state = np.zeros(A.shape[0])
+        if target is not None:
+            self.target_state[:target.shape[0]] = target
         K, S, E = lqr(A,B,Q,R)
         StateFeedback.__init__(self, intopic, outtopic, K)
 
@@ -34,6 +46,7 @@ if __name__ == "__main__":
 
     n = rospy.get_param("dynamaestro/output_dim")
     m = rospy.get_param("dynamaestro/number_integrators")
+    prob = integrator_chains.Problem.loadJSON(rospy.get_param("dynamaestro/probleminstance"))
 
     A = np.diag(np.ones((m-1)*n), k=n)
     B = np.zeros((m*n, n))
@@ -41,5 +54,18 @@ if __name__ == "__main__":
     Q = np.eye(m*n)
     R = np.eye(n)
 
-    lqrc = LQRController("input", "output", A, B, Q, R)
-    rospy.spin()
+    targets = []
+    for goal in prob.goals:
+        target_polytopeV = goal.getVrep()
+        targets.append( np.mean(target_polytopeV, axis=0) );
+
+    current = 0
+    while not rospy.is_shutdown():
+        lqrc = LQRController("input", "output", A, B, targets[current], Q, R)
+        while (not rospy.is_shutdown()
+               and ((lqrc.error is None) or (la.norm(lqrc.error) > 0.01))):
+            time.sleep(0.5)
+        lqrc.unregister()
+        current += 1
+        if current >= len(prob.goals):
+            current = 0
---
+++ tools/fmrb-pkg/fmrb/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = '0.0.0-dev0+Unknown'
