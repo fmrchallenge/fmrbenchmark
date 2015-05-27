d5745f6c9d8a9f64510e9d3dea6d368ad7698185
Scott Livingston <slivingston@cds.caltech.edu>
2015-05-27 08:51:22 -0700

    First domain, per trial state and input vs. time plotting

--- domains/integrator_chains/analysis/tdstat.py
+++ domains/integrator_chains/analysis/tdstat.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import sys
 import argparse
 import json
+import numpy as np
 import matplotlib.pyplot as plt
 
 from fmrb import integrator_chains
@@ -77,6 +78,20 @@ if __name__ == '__main__':
     parser.add_argument('-p', action='store_true',
                         dest='get_probinstance', default=False,
                         help='extract problem instance JSON for trial.')
+    parser.add_argument('--state', type=str, metavar='I',
+                        dest='state_indices', default='',
+                        help=('comma-separated list of indices of state, '
+                              'or "*" to indicate entire state vector.'))
+    parser.add_argument('--input', type=str, metavar='I',
+                        dest='input_indices', default='',
+                        help=('comma-separated list of indices of input, '
+                              'or "*" to indicate entire input vector.'))
+    parser.add_argument('--time', action='store_true',
+                        dest='plot_timeseries', default=False,
+                        help=('create timeseries plot. Parts of the state '
+                              'and input to include can be selected using '
+                              'the `--state` and `--input` switches, '
+                              'respectively.'))
     args = parser.parse_args()
 
     with open(args.FILE, 'r') as f:
@@ -93,5 +108,73 @@ if __name__ == '__main__':
 
     if args.get_probinstance:
         print(json.dumps(td['trials'][args.T]['problem_instance']))
+        sys.exit(0)
+
+    prob = integrator_chains.Problem.loadJSONdict(td['trials'][args.T]['problem_instance'])
+    state_dim = prob.output_dim*prob.number_integrators
+
+    if args.plot_timeseries:
+        def get_index_list(indices_str, bounds):
+            if indices_str == '*':
+                indices = range(bounds[0], bounds[1]+1)
+            else:
+                indices = [bounds[0]+int(k) for k in indices_str.split(',')]
+                if (min(indices) < bounds[0]
+                    or max(indices) > bounds[1]):
+                    return None
+            return indices
+
+        try:
+            traj = np.array(td['trials'][args.T]['trajectory'])
+        except KeyError:
+            print('Trial '+str(args.T)+' does not have an associated trajectory.')
+            sys.exit(-1)
+
+        if args.state_indices.strip() == '':
+            state_indices = []
+        else:
+            state_index_bounds = (2+prob.output_dim, 2+prob.output_dim+state_dim-1)
+            state_indices = get_index_list(args.state_indices, state_index_bounds)
+            if state_indices is None:
+                print('One of the requested state indices is out of bounds, '
+                      '[0, '+str(state_index_bounds[1]-state_index_bounds[0])+']')
+                sys.exit(-1)
+
+        if args.input_indices.strip() == '':
+            input_indices = []
+        else:
+            input_index_bounds = (2, 2+prob.output_dim-1)
+            input_indices = get_index_list(args.input_indices, input_index_bounds)
+            if input_indices is None:
+                print('One of the requested input indices is out of bounds, '
+                      '[0, '+str(input_index_bounds[1]-input_index_bounds[0])+']')
+                sys.exit(-1)
+
+        if len(state_indices) == 0 and len(input_indices) == 0:
+            print('Try `--state` and `--input` options to select indices for plotting.')
+            sys.exit(0)
+
+        t = traj[:,0]+traj[:,1]*1e-9
+        t -= t[0]
+        if len(state_indices) > 0:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.hold(True)
+            for state_ind in state_indices:
+                ax.plot(t, traj.T[state_ind])
+            ax.set_title('state')
+            ax.set_xlabel('Time (s)')
+            ax.legend([str(k-state_index_bounds[0]) for k in state_indices])
+        if len(input_indices) > 0:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.hold(True)
+            for state_ind in input_indices:
+                ax.plot(t, traj.T[state_ind])
+            ax.set_title('input')
+            ax.set_xlabel('Time (s)')
+            ax.legend([str(k-input_index_bounds[0]) for k in input_indices])
+        plt.show()
+
     else:
         print(json.dumps(td['trials'][args.T]))
