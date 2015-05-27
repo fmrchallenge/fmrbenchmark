5c54030dd456d8e43740cd732917a0624b3c7ced
Scott Livingston <slivingston@cds.caltech.edu>
2015-05-27 15:34:06 -0700

    EXAMPLE: Update genproblem for current Problem::random()
    The output can be plotted as before, e.g.,
      ./genproblem | ../../analysis/plotp.py -

--- domains/integrator_chains/dynamaestro/examples/standalone/genproblem.cpp
+++ domains/integrator_chains/dynamaestro/examples/standalone/genproblem.cpp
@@ -11,18 +11,24 @@ int main()
 	Eigen::Vector2i numdim_output_bounds( 2, 2 );
 	Eigen::Vector2i num_integrators_bounds( 2, 2 );
 
-	Eigen::Vector4d Y_max;
-	Y_max << 0, 10, 0, 10;
+	Eigen::Vector4d Y_box;
+	Y_box << -1, 10, -2, 10;
 
-	Eigen::Vector4d U_max;
-	U_max << -1, 1, -1, 1;
+	Eigen::Vector4d U_box;
+	U_box << -1, 1, -1, 1;
 
 	Eigen::Vector2d period_bounds( 0.05, 0.1 );
+
+	Eigen::Vector2i number_goals_bounds( 1, 2 );
+	Eigen::Vector2i number_obstacles_bounds( 0, 1 );
 
 	srand(time(0));
 	Problem *prob = Problem::random( numdim_output_bounds,
 									 num_integrators_bounds,
-									 Y_max, U_max, 2, 1, period_bounds );
+									 Y_box, U_box,
+									 number_goals_bounds,
+									 number_obstacles_bounds,
+									 period_bounds );
 	std::cout << *prob << std::endl;
 
 	prob->to_formula( std::cerr );
