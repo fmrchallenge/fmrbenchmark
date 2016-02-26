851b41e2774978ed6598e772981df833c6ec7b60
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-02-26 08:32:34 -0500

    First draft road description file

--- domains/dubins_traffic/dub_sim/genroad.py
+++ domains/dubins_traffic/dub_sim/genroad.py
@@ -3,6 +3,43 @@ from __future__ import print_function
 import math
 import argparse
 
+
+class RoadNetwork(object):
+    def __init__(self, rnd_file):
+        """
+
+        rnd_file can be string (file name) or file-like object.
+        """
+        if getattr(rnd_file, 'readline', None) is None:
+            rnd_file = open(rnd_file, 'rt')
+
+        self.version = None
+        self.length = None
+        self.segments = list()
+
+        reading_segments = False
+        for line in rnd_file:
+            line = line.strip()
+            if len(line) == 0 or line.startswith('#'):
+                continue  # Ignore comments, blank lines
+            comment_index = line.find('#')
+            if comment_index > 0:
+                line = line[:comment_index]  # Strip trailing comments
+            if not reading_segments:
+                if line.startswith('version'):
+                    self.version = int(line.split(':')[1])
+                    assert self.version == 0, 'Unrecognized format version'
+                elif line.startswith('length'):
+                    self.length = float(line.split(':')[1])
+                    assert self.length > 0, 'Unit side length must be positive'
+                elif line.startswith('segments'):
+                    reading_segments = True
+                else:
+                    raise ValueError('Unrecognized file entry.')
+            else:
+                self.segments.append([int(x) for x in line.split()])
+
+
 def road_segment(x1, x2, prefix='straightroad'):
     assert len(x1) == 2 and len(x2) == 2
     
@@ -49,9 +86,8 @@ if __name__ == '__main__':
     parser.add_argument('FILE', type=str, help='road network description file')
     args = parser.parse_args()
 
-    segments = [[-2.5, 0, 5, 0],
-                [5, 0, 5, 5],
-                [-2.5, 0, -2.5, -4]]
+    with open(args.FILE, 'rt') as f:
+        roads = RoadNetwork(f)
 
     print("""<?xml version="1.0" ?>
 <sdf version="1.4">
@@ -61,7 +97,7 @@ if __name__ == '__main__':
     <include><uri>model://sun</uri></include>
     <include><uri>model://ground_plane</uri></include>
 """)
-    for i, segment in enumerate(segments):
-        print(road_segment((segment[0], segment[1]),
-                           (segment[2], segment[3]), prefix='segment_'+str(i)+'_'))
+    for i, segment in enumerate(roads.segments):
+        print(road_segment((segment[0]*roads.length, segment[1]*roads.length),
+                           (segment[2]*roads.length, segment[3]*roads.length), prefix='segment_'+str(i)+'_'))
     print('</world></sdf>')
