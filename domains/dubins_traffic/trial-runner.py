92b5694d298aa5ad8610e871af1c8092205881f8
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-04-16 09:51:52 -0400

    Launch two "wander" e-agents via trial-runner.py

--- domains/dubins_traffic/trial-runner.py
+++ domains/dubins_traffic/trial-runner.py
@@ -3,6 +3,7 @@
 """
 from __future__ import print_function
 import argparse
+import random
 import json
 import tempfile
 import subprocess
@@ -11,7 +12,7 @@ import os
 from fmrb import dubins_traffic
 
 
-def gen_roslaunch(worldsdf_filename):
+def gen_roslaunch(worldsdf_filename, rnd_path):
     nl = '\n'
     idt = ' '*2
     output = '<launch>'+nl
@@ -23,16 +24,22 @@ def gen_roslaunch(worldsdf_filename):
   </include>
 
   <param name="robot_description" command="$(find xacro)/xacro.py '$(find dub_sim)/urdf/lasermounted.urdf.xacro'" />
-
-  <include file="$(find dub_sim)/launch/includes/scopedbase.launch.xml">
-    <arg name="namespace" value="vehicle0" />
-    <arg name="init_pose" value="-x -2 -y -0.3 -z 0 -Y 0" />
-  </include>
-  <include file="$(find dub_sim)/launch/includes/scopedbase.launch.xml">
-    <arg name="namespace" value="vehicle1" />
-    <arg name="init_pose" value="-x 2 -y 0.3 -z 0 -Y 3.14159" />
-  </include>
 """
+    for eagent_index in range(2):
+        output += """
+  <include file="$(find dub_sim)/launch/includes/scopedbase.launch.xml">
+    <arg name="namespace" value="vehicle{EAGENT_INDEX}" />
+    <arg name="init_pose" value="-x {X} -y {Y} -z 0 -Y 0" />
+  </include>
+  <node pkg="wander" type="wander"
+   name="$(anon vehicle{EAGENT_INDEX})" ns="vehicle{EAGENT_INDEX}"
+   args="{RNDPATH}">
+    <remap from="cmd_vel" to="mobile_base/commands/velocity" />
+  </node>
+""".format(EAGENT_INDEX=eagent_index,
+           X=int(random.random()*10), Y=int(random.random()*10),
+           RNDPATH=rnd_path)
+
     return output+nl+'</launch>'
 
 
@@ -51,7 +58,7 @@ if __name__ == '__main__':
 
     tempfd, tempfname = tempfile.mkstemp()
     launchfile = os.fdopen(tempfd, 'w+')
-    launchfile.write(gen_roslaunch(tempfname_sdf))
+    launchfile.write(gen_roslaunch(tempfname_sdf, rnd_path=args.FILE))
     launchfile.seek(0)
     try:
         launchp = subprocess.Popen(['roslaunch', '-'], stdin=launchfile)
