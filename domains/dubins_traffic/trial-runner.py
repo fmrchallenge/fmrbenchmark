38b043c7a758b565140d321045c02b721e57ced7
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-04-17 08:34:45 -0400

    DOC: trial-runner description indicates problem domain

--- domains/dubins_traffic/trial-runner.py
+++ domains/dubins_traffic/trial-runner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Run trials; collect results.
+"""Run trials and collect results for the Problem domain: Traffic network of Dubins cars.
 """
 from __future__ import print_function
 import argparse
--- domains/integrator_chains/trial-runner.py
+++ domains/integrator_chains/trial-runner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Run trials; collect results.
+"""Run trials and collect results for the Problem domain: Scaling chains of integrators.
 """
 
 from __future__ import print_function
