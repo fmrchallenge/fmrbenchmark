a99056a497c6babdc3171b3fd3587e54934ab6a0
Scott C. Livingston <slivingston@cds.caltech.edu>
2016-04-22 21:16:41 -0400

    monitor: get RND JSON and e-agent list from Parameter Server

--- domains/dubins_traffic/dubins_traffic_utils/src/monitor.cpp
+++ domains/dubins_traffic/dubins_traffic_utils/src/monitor.cpp
@@ -3,8 +3,50 @@
  * SCL; 2016
  */
 
+#include <ros/ros.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unistd.h>
+
+#include "roadnet.hpp"
+
 
 int main( int argc, char **argv )
 {
+    ros::init( argc, argv, "dubins_traffic_monitor" );
+    ros::NodeHandle nh( "~" );
+
+    std::string rndjson;
+    while (!nh.getParam( "/dubins_traffic/rnd", rndjson )) {
+        ROS_INFO( "dubins_traffic monitor: RND JSON string not present; will try again soon..." );
+        sleep( 1 );
+        if (!ros::ok())
+            return 0;
+    }
+
+    std::string eagents_str;
+    while (!nh.getParam( "/dubins_traffic/e_agents", eagents_str )) {
+        ROS_INFO( "dubins_traffic monitor: e-agents list not present; will try again soon..." );
+        sleep( 1 );
+        if (!ros::ok())
+            return 0;
+    }
+    std::vector<std::string> eagent_names;
+    if (eagents_str.size() > 0) {
+        size_t pos = 0, nextpos;
+        while (true) {
+            nextpos = eagents_str.find( ",", pos );
+            if (nextpos == std::string::npos) {
+                eagent_names.push_back( eagents_str.substr( pos ) );
+                break;
+            }
+            eagent_names.push_back( eagents_str.substr( pos, nextpos-pos ) );
+            pos = nextpos + 1;
+        }
+
+    }
+
     return 0;
 }
